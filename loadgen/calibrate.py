a01e89ba06ad5f106bf689462efda4b0de8e5998
Panagiotis Stefanis <el19096@mail.ntua.gr>
2026-02-27 03:09:20 +0200

    we are so fucking back

--- loadgen/calibrate.py
+++ loadgen/calibrate.py
@@ -5,13 +5,24 @@ import subprocess
 import os
 import socket
 
+def get_reps(arg):
+    if arg <= 34: return 300  # Captures jitter for short-lived tasks
+    if arg <= 39: return 30   # Reliable mean for medium tasks
+    if arg <= 42: return 5    # Minimal noise for long tasks
+    return 2                 # Ground truth for very long tasks
+
 # The function to launch the C++ fibonacci function
 def launch_command_cpp(arg):
-    command = (
-        f"./payload/launch_function.out {arg}"
-    )
-    print(command)
-    subprocess.run(command, shell=True)
+    command = [
+        "/shared/loadgen/payload/launch_function.out",
+        str(arg)
+    ]
+    subprocess.run(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
 
 
 dur_list = []
@@ -20,10 +31,10 @@ fib = []
 
 # Measure the runtime of the C++ fibonacci function
 def loop(arg, repeat):
-    start = time.time()
+    start = time.perf_counter()
     for i in range(repeat):
         launch_command_cpp(arg)
-    end = time.time()
+    end = time.perf_counter()
     # get milliseconds
     print("Runtime for arg {} is {} ms".format(arg, (end - start) * 1000 / repeat))
     dur_list.append(round((end - start) * 1000 / repeat))
@@ -38,11 +49,9 @@ if __name__ == "__main__":
     if not os.path.exists("./log"):
         os.makedirs("./log")
 
-    for i in range(3, 41):
-        loop(i, 100)
+    for i in range(24, 47):
+        loop(i, get_reps(i))
 
-    for i in range(40, 47):
-        loop(i, 20)
 
     with open(f"./log/calibrate_list_{socket.gethostname()}_{date.today()}.txt", "w") as f:
         f.write("dur_list = {}\n".format(dur_list))
